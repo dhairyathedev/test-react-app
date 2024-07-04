d6cb12f78f25df33b2a4a369ecf770c344e06bd8
Dhairya  Shah <dtshah24022006@gmail.com>
2024-07-04 23:46:35 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import { IconBrandRust } from '@tabler/icons-react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <IconBrandRust stroke={1.5} className="w-52 h-52 text-red-600 animate-bounce"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vercel Clone is working!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dhairyashah.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more
         </a>
       </header>
     </div>
